ec066ee6520ac814f03e534aa7737a5198420270
insmirnova <60112547+insmirnova@users.noreply.github.com>
2020-03-20 22:18:00 +0400

    защита

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 var buttonCloseForm = document.querySelector(".button-close-form");
 var searchForm = document.querySelector(".search-form");
 var required = document.querySelectorAll(".required");
-<<<<<<< HEAD
 var countOfPeople = document.querySelectorAll(".input-number");
 
 searchForm.classList.add("search-form-hidden");
@@ -23,22 +22,6 @@ searchForm.addEventListener("submit", function(evt) {
         searchForm.classList.remove("form-error");
         searchForm.offsetWidth = searchForm.offsetWidth;
         searchForm.classList.add("form-error");
-=======
-var countOfPeople = document.querySelector(".input-number");
-
-buttonCloseForm.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    searchForm.classList.toggle("search-form-hidden")
-})
-
-searchForm.addEventListener("submit", function(evt) {
-    var success = true;
-    required.forEach(function(elem) {
-        if (!elem.value) success = false;
-    })
-    if (!success) {
-        evt.preventDefault();
->>>>>>> 220fcbedf93b10694cd305a2907b284e685dac63
         console.log("Необходимо заполнить все поля");
     }
     else {
